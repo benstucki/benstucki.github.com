93410bfcc56faa827b09b3d5b0c4726fb2481426
Ben Stucki <benstucki@gmail.com>
2012-08-27 18:55:40 -0500

    removed themes folder and added legacy querystring support

---
+++ source/javascripts/legacy.js
@@ -0,0 +1,11 @@
+var queries = location.search.substring(1, location.search.length).split('&');
+for(var i=0; i<queries.length; i++) {
+	var query = queries[i].split('=');
+	if(query[0]=='p' || query[0]=='id') {
+		$.ajax({url:"posts.json", dataType:"json", success: function(data) {
+			var count = data.posts.length;
+			var url = data.posts[Number(count-query[1]-1)];
+			window.location = "http://benstucki.com" + url;
+		}})
+	}
+}
